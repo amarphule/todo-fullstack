af93b3e54d7e0fccbc048dd0a9bf2a302d1c3969
Amar Phule <amardeepphule@gmail.com>
2024-07-05 15:39:42 +0530

    feat: handle empty check

--- frontend/src/components/CreateTodo.jsx
+++ frontend/src/components/CreateTodo.jsx
@@ -3,9 +3,16 @@ import { useState } from "react";
 const CreateTodo = ({ setRefreshKey }) => {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
+  const [error, setError] = useState(false);
 
   const handleAddTodo = async () => {
+    if (!(title && description)) {
+      setError(true);
+      return;
+    }
     try {
+      setError(false);
+
       const resp = await fetch("http://localhost:3000/todo", {
         method: "POST",
         headers: {
@@ -19,11 +26,21 @@ const CreateTodo = ({ setRefreshKey }) => {
       }
       setTitle("");
       setDescription("");
+      setError(false);
       setRefreshKey((prev) => prev + 1);
       console.log("Successfully created todo");
     } catch (error) {
       console.log("Error creating todo ", error);
     }
+  };
+
+  const handleTitleChange = (e) => {
+    setTitle(e.target.value);
+    setError(false);
+  };
+  const handleDescriptionChange = (e) => {
+    setDescription(e.target.value);
+    setError(false);
   };
   return (
     <div className="container">
@@ -31,16 +48,17 @@ const CreateTodo = ({ setRefreshKey }) => {
         className="box"
         type="text"
         placeholder="Todo Title"
-        onChange={(e) => setTitle(e.target.value)}
+        onChange={handleTitleChange}
         value={title}
       />
       <input
         className="box"
         type="text"
         placeholder="Todo Description"
-        onChange={(e) => setDescription(e.target.value)}
+        onChange={handleDescriptionChange}
         value={description}
       />
+      {error && <span style={{ color: "red" }}>All fields are required</span>}
       <button className="btn" onClick={handleAddTodo}>
         Add TODO
       </button>
